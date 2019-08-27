c91411ff86735e93b6159029aeda8fda43631ecf
irobaina <irobaina@tritansoftware.com>
2019-08-27 15:55:24 -0400

    adding current price calculated

--- src/containers/BurgerBuilder/BurguerBuilder.js
+++ src/containers/BurgerBuilder/BurguerBuilder.js
@@ -51,12 +51,20 @@ class BurgerBuilder extends Component {
     }
 
     render() {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
                     ingredientAdded={this.addIngredientsHandle}
-                    ingredientRemove={this.removeIngredientsHandle} />
+                    ingredientRemove={this.removeIngredientsHandle}
+                    disabled={disabledInfo}
+                    price={this.state.totalPrice} />
             </Aux>
         )
     }
