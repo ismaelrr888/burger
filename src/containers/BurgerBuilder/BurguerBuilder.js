4989d32f766c57dae1e7a4f6d627f373ea6cb727
irobaina <irobaina@tritansoftware.com>
2019-08-27 16:33:10 -0400

    disabled and enabled order button

--- src/containers/BurgerBuilder/BurguerBuilder.js
+++ src/containers/BurgerBuilder/BurguerBuilder.js
@@ -19,7 +19,19 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchaseable: false
+    }
+
+    updatePurchaseState(ingredients) {
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+                return ingredients[igKey];
+            })
+            .reduce((sum, el) => {
+                return sum + el
+            }, 0);
+        this.setState({ purchaseable: sum > 0 });
     }
 
     addIngredientsHandle = (type) => {
@@ -33,6 +45,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
         this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+        this.updatePurchaseState(updatedIngredients);
     }
     removeIngredientsHandle = (type) => {
         const oldCount = this.state.ingredients[type];
@@ -48,6 +61,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceDeduction;
         this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+        this.updatePurchaseState(updatedIngredients);
     }
 
     render() {
@@ -64,6 +78,7 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addIngredientsHandle}
                     ingredientRemove={this.removeIngredientsHandle}
                     disabled={disabledInfo}
+                    purchaseable={this.state.purchaseable}
                     price={this.state.totalPrice} />
             </Aux>
         )
