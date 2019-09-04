d8a5739c8d5ea767013eef7e7f1557f013a1a451
irobaina <irobaina@tritansoftware.com>
2019-09-03 20:06:13 -0400

    Add navigation checkout and summary

--- src/containers/BurgerBuilder/BurguerBuilder.js
+++ src/containers/BurgerBuilder/BurguerBuilder.js
@@ -84,28 +84,16 @@ class BurgerBuilder extends Component {
     }
     purchaseContinueHandle = () => {
         //alert('You continue')
-        this.setState({ loading: true });
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Ismael Robaian',
-                address: {
-                    street: '64 street',
-                    zipCode: '21600',
-                    country: 'Cuba'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false, purchasing: false });
-            })
-            .catch(error => {
-                this.setState({ loading: false, purchasing: false });
-            });
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     }
 
     render() {
