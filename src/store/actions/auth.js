c440f71c5420ef1c132962b37b8c67684ba8a943
irobaina <irobaina@tritansoftware.com>
2019-10-08 17:45:29 -0400

    add singnin + singnup

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -20,8 +22,26 @@ export const authFail = (error) => {
     };
 }
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCIW7rJToMsBVSHUgsq0kzZXXDezDuPhQg';
+        if (!isSignup) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCIW7rJToMsBVSHUgsq0kzZXXDezDuPhQg';
+        }
+        axios.post(url, authData)
+            .then(response => {
+                console.log(response)
+                dispatch(authSuccess(response.data))
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            })
     };
 };
